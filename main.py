da7db95dab0bad323d55572d143c077dc910c52b
Akshey <55742659+akshey-kumar@users.noreply.github.com>
2024-06-04 19:01:40 +0200

    changed indentation to spaces

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ from ncmcm.visualisers.latent_space import LatentSpaceVisualiser
 worm_num = 0
 algorithm = 'BunDLeNet'
 b_neurons = [
-	'AVAR',
-	'AVAL',
-	'SMDVR',
-	'SMDVL',
-	'SMDDR',
-	'SMDDL',
-	'RIBR',
-	'RIBL'
+    'AVAR',
+    'AVAL',
+    'SMDVR',
+    'SMDVL',
+    'SMDDR',
+    'SMDDL',
+    'RIBR',
+    'RIBL'
 ]
 data_path = 'datasets/raw/NoStim_Data.mat'
 data = Database(data_path=data_path, dataset_no=worm_num)
@@ -33,21 +33,21 @@ X_, B_ = prep_data(X, B, win=15)
 model = BunDLeNet(latent_dim=3, num_behaviour=len(data.behaviour_names))
 model.build(input_shape=X_.shape)
 loss_array, _ = train_model(
-	X_,
-	B_,
-	model,
-	b_type='discrete',
-	gamma=0.9,
-	learning_rate=0.001,
-	n_epochs=1000
+    X_,
+    B_,
+    model,
+    b_type='discrete',
+    gamma=0.9,
+    learning_rate=0.001,
+    n_epochs=1000
 )
 
 for i, label in enumerate([
-	"$\mathcal{L}_{{Markov}}$",
-	"$\mathcal{L}_{{Behavior}}$",
-	"Total loss $\mathcal{L}$"
+    "$\mathcal{L}_{{Markov}}$",
+    "$\mathcal{L}_{{Behavior}}$",
+    "Total loss $\mathcal{L}$"
 ]):
-	plt.plot(loss_array[:, i], label=label)
+    plt.plot(loss_array[:, i], label=label)
 plt.legend()
 plt.show()
 
@@ -57,12 +57,12 @@ Y0_ = model.tau(X_[:, 0]).numpy()
 # Save the weights
 save_model = False
 if save_model:
-	algorithm = 'BunDLeNet'
-	model.save_weights('data/generated/BunDLeNet_model_worm_' + str(worm_num))
-	np.savetxt('data/generated/saved_Y/Y0__' + algorithm + '_worm_' + str(worm_num), Y0_)
-	np.savetxt('data/generated/saved_Y/B__' + algorithm + '_worm_' + str(worm_num), B_)
-	Y0_ = np.loadtxt('data/generated/saved_Y/Y0__' + algorithm + '_worm_' + str(worm_num))
-	B_ = np.loadtxt('data/generated/saved_Y/B__' + algorithm + '_worm_' + str(worm_num)).astype(int)
+    algorithm = 'BunDLeNet'
+    model.save_weights('data/generated/BunDLeNet_model_worm_' + str(worm_num))
+    np.savetxt('data/generated/saved_Y/Y0__' + algorithm + '_worm_' + str(worm_num), Y0_)
+    np.savetxt('data/generated/saved_Y/B__' + algorithm + '_worm_' + str(worm_num), B_)
+    Y0_ = np.loadtxt('data/generated/saved_Y/Y0__' + algorithm + '_worm_' + str(worm_num))
+    B_ = np.loadtxt('data/generated/saved_Y/B__' + algorithm + '_worm_' + str(worm_num)).astype(int)
 
 # Plotting latent space dynamics
 vis = LatentSpaceVisualiser(Y0_, B_, data.behaviour_names)
