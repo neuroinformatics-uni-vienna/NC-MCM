ade221ac57a9c1b17b1bc2cd4639c0c687cefe33
DriftKing1998 <hofer.michael.98@gmail.com>
2024-06-07 22:45:20 +0200

    Update test_cognitive_graphs.py

--- ncmcm/cognitive_graphs/tests/test_cognitive_graphs.py
+++ ncmcm/cognitive_graphs/tests/test_cognitive_graphs.py
@@ -1,6 +1,9 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 import numpy as np
+from ncmcm.cognitive_graphs.calculations import *
+from ncmcm.cognitive_graphs.custom_models import *
+from ncmcm.cognitive_graphs.helpers import *
 from ncmcm.cognitive_graphs.cognitive_graphs import behavioral_state_diagram, cluster_neural_activity
 from unittest.mock import patch
 
