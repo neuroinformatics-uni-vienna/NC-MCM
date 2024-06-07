61279dd19a59546d0054c9bd87cac0a6d2211b19
DriftKing1998 <hofer.michael.98@gmail.com>
2024-06-07 23:05:23 +0200

    test

--- ncmcm/cognitive_graphs/tests/test_cognitive_graphs.py
+++ ncmcm/cognitive_graphs/tests/test_cognitive_graphs.py
@@ -4,10 +4,11 @@ import numpy as np
 from ncmcm.cognitive_graphs.calculations import *
 from ncmcm.cognitive_graphs.custom_models import *
 from ncmcm.cognitive_graphs.helpers import *
-#from ncmcm.cognitive_graphs.cognitive_graphs import behavioral_state_diagram, cluster_neural_activity
-#from unittest.mock import patch
-#
-#
+#from ncmcm.cognitive_graphs.cognitive_graphs import behavioral_state_diagram
+from ncmcm.cognitive_graphs.cognitive_graphs import cluster_neural_activity
+from unittest.mock import patch
+
+
 # def test_behavioral_state_diagram():
 #     cognitive_states = [np.random.randint(0, 2) for _ in range(100)]
 #     actions = ['sit', 'stand', 'walk', 'run']
@@ -72,35 +73,35 @@ from ncmcm.cognitive_graphs.helpers import *
 #                                       test=True,
 #                                       bins=10)
 #     assert result is True
-#
-#
-# def test_cluster_neural_activity():
-#     n = np.random.uniform(low=0.0, high=3.0, size=(15, 100))
-#     actions = ['sit', 'stand', 'walk', 'run']
-#     behaviors = [np.random.choice(actions) for _ in range(100)]
-#
-#     res = cluster_neural_activity(n, behaviors,
-#                                   n_clusters=3,
-#                                   nrep=2,
-#                                   stationary=True,
-#                                   model=RandomForestClassifier(n_estimators=3,
-#                                                                random_state=42))
-#
-#     assert len(res) == 2
-#     assert len(res[0]) == 3
-#     assert len(res[0][0]) == 100
-#     assert len(np.unique(res[0][0])) == 3
-#     assert 0 <= res[0][1] <= 1
-#     assert 0 <= res[0][2] <= 1
-#
-#     res = cluster_neural_activity(n, behaviors,
-#                                   n_clusters=4,
-#                                   nrep=3,
-#                                   stationary=False,
-#                                   model=LogisticRegression())
-#
-#     assert len(res) == 3
-#     assert len(res[0]) == 2
-#     assert len(res[0][0]) == 100
-#     assert len(np.unique(res[0][0])) == 4
-#     assert 0 <= res[0][1] <= 1
+
+
+def test_cluster_neural_activity():
+    n = np.random.uniform(low=0.0, high=3.0, size=(15, 100))
+    actions = ['sit', 'stand', 'walk', 'run']
+    behaviors = [np.random.choice(actions) for _ in range(100)]
+
+    res = cluster_neural_activity(n, behaviors,
+                                  n_clusters=3,
+                                  nrep=2,
+                                  stationary=True,
+                                  model=RandomForestClassifier(n_estimators=3,
+                                                               random_state=42))
+
+    assert len(res) == 2
+    assert len(res[0]) == 3
+    assert len(res[0][0]) == 100
+    assert len(np.unique(res[0][0])) == 3
+    assert 0 <= res[0][1] <= 1
+    assert 0 <= res[0][2] <= 1
+
+    res = cluster_neural_activity(n, behaviors,
+                                  n_clusters=4,
+                                  nrep=3,
+                                  stationary=False,
+                                  model=LogisticRegression())
+
+    assert len(res) == 3
+    assert len(res[0]) == 2
+    assert len(res[0][0]) == 100
+    assert len(np.unique(res[0][0])) == 4
+    assert 0 <= res[0][1] <= 1
