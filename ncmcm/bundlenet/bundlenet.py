8ae772fb1ce42d316c0fdc36d9110ce50eeacc58
Akshey <55742659+akshey-kumar@users.noreply.github.com>
2024-07-03 18:36:30 +0200

    adapted the tf code of bundle net so everything can run on the latest version of python and tf
    had to substantially change some syntax in bundle net to allow it to work on tf 2.16. Had to slightly modify some tests as well since the syntax in tf 2.16 differs so much for tf 2.13.
    
    i also adapted the setup.py so everything works with minimal requirements (and latest)

--- ncmcm/bundlenet/bundlenet.py
+++ ncmcm/bundlenet/bundlenet.py
@@ -47,12 +47,10 @@ class BunDLeNet(Model):
             layers.Dense(50, activation='relu'),
             layers.Dense(10, activation='relu'),
             layers.Dense(latent_dim, activation='linear'),
-            layers.Normalization(axis=-1),
             layers.GaussianNoise(0.05)
         ])
         self.T_Y = tf.keras.Sequential([
             layers.Dense(latent_dim, activation='linear'),
-            layers.Normalization(axis=-1),
         ])
         self.predictor = tf.keras.Sequential([
             layers.Dense(num_behaviour, activation='linear')
@@ -69,6 +67,20 @@ class BunDLeNet(Model):
 
         return yt1_upper, yt1_lower, bt1_upper
 
+    def get_config(self):
+        config = {
+            'latent_dim': self.latent_dim,
+            'num_behaviour': self.num_behaviour,
+        }
+        base_config = super(BunDLeNet, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(
+            latent_dim=config['latent_dim'],
+            num_behaviour=config['num_behaviour'],
+        )
 
 class BunDLeTrainer:
     """
@@ -159,15 +171,15 @@ def train_model(x_train, b_train_1, model, b_type, gamma, learning_rate, n_epoch
         numpy.ndarray: Array of loss values during training.
     """
     train_dataset = tf_batch_prep(x_train, b_train_1)
-    optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=learning_rate)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
     if validation_data is not None:
-        X_test, B_test_1 = validation_data
-        test_dataset = tf_batch_prep(X_test, B_test_1)
+        x_test, b_test_1 = validation_data
+        test_dataset = tf_batch_prep(x_test, b_test_1)
 
     if initialisation == 'pca_init':
         pca_initialisation(x_train, model.tau, model.latent_dim)
-        model.tau.load_weights('temp/tau_pca_weights.h5')
+        model.tau.load_weights('temp/tau_pca.weights.h5')
     elif initialisation == 'best_of_5_init':
         model = best_of_5_runs(x_train, b_train_1, model, b_type, gamma, learning_rate, validation_data)
     elif initialisation is None:
