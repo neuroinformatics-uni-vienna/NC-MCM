4359adfd24c1f990a269062d980d55cdc4f96c08
Akshey <55742659+akshey-kumar@users.noreply.github.com>
2025-02-20 16:01:00 +0100

    Revert "finishing up visualiser.ipynb & option to return encoder or file path…"

--- ncmcm/bundlenet/bundlenet.py
+++ ncmcm/bundlenet/bundlenet.py
@@ -17,10 +17,10 @@ from .utils import torch_batch_prep, GaussianNoise
 class BunDLeNet(nn.Module):
     """Behaviour and Dynamical Learning Network (BunDLeNet) model.
 
-    This model represents the BunDLe Net's architecture for deep learning and 
-    is based on the commutativity diagrams. The resulting model is dynamically 
-    consistent (DC) and behaviourally consistent (BC) as per the notion 
-    described in the paper.
+    This model represents the BunDLe Net's architecture for deep learning and is
+    based on the commutativity diagrams. The resulting model is dynamically 
+    consistent (DC) and behaviourally consistent (BC) as per the notion described
+    in the paper.
     
     Args:
         latent_dim (int):
@@ -37,12 +37,7 @@ class BunDLeNet(nn.Module):
 
     """
 
-    def __init__(
-        self, 
-        latent_dim: int, 
-        num_behaviour: int, 
-        input_shape: tuple
-    ):
+    def __init__(self, latent_dim: int, num_behaviour: int, input_shape: tuple):
         super(BunDLeNet, self).__init__()
         in_features = np.prod(input_shape[-2:])
         self.latent_dim = latent_dim
@@ -78,7 +73,7 @@ class BunDLeNet(nn.Module):
         yt1_lower = yt_lower + self.T_Y(yt_lower)
 
         return yt1_upper, yt1_lower, bt1_upper
-    
+
     def get_config(self):
         config = {
             'latent_dim': self.latent_dim,
@@ -86,7 +81,7 @@ class BunDLeNet(nn.Module):
         }
         base_config = super(BunDLeNet, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
-    
+
     @classmethod
     def from_config(cls, config):
         return cls(
@@ -113,7 +108,7 @@ class BunDLeTrainer:
         self.optimizer = optimizer
         self.gamma = gamma
         self.bccdcc_loss = BccDccLoss(b_type, gamma)
-    
+
     def train_step(self, x_train, b_train_1):
         self.model.train()
         self.optimizer.zero_grad()
@@ -121,18 +116,13 @@ class BunDLeTrainer:
         # forward pass
         yt1_upper, yt1_lower, bt1_upper = self.model(x_train)
         # loss calculation
-        dcc_loss, behaviour_loss, total_loss = self.bccdcc_loss(
-            yt1_upper, 
-            yt1_lower, 
-            bt1_upper,
-            b_train_1
-        )
+        dcc_loss, behaviour_loss, total_loss = self.bccdcc_loss(yt1_upper, yt1_lower, bt1_upper, b_train_1)
 
         total_loss.backward()
         self.optimizer.step()
 
         return dcc_loss.item(), behaviour_loss.item(), total_loss.item()
-    
+
     def test_step(self, x_test, b_test_1):
         self.model.eval()
 
@@ -141,70 +131,39 @@ class BunDLeTrainer:
             yt1_upper, yt1_lower, bt1_upper = self.model(x_test)
 
         # loss calculation
-        dcc_loss, behaviour_loss, total_loss = self.bccdcc_loss(
-            yt1_upper, 
-            yt1_lower, 
-            bt1_upper, 
-            b_test_1
-        )
+        dcc_loss, behaviour_loss, total_loss = self.bccdcc_loss(yt1_upper, yt1_lower, bt1_upper, b_test_1)
 
         return dcc_loss.item(), behaviour_loss.item(), total_loss.item()
-    
+
     def train_loop(self, train_loader):
         """
         Handles the training within a single epoch and logs losses
         """
         loss_array = np.zeros((0, 3))
         for x_train, b_train_1 in train_loader:
-            dcc_loss, behaviour_loss, total_loss = self.train_step(
-                x_train, 
-                b_train_1
-            )
-            loss_array = np.append(
-                loss_array, 
-                [[dcc_loss, behaviour_loss, total_loss]], 
-                axis=0
-            )
+            dcc_loss, behaviour_loss, total_loss = self.train_step(x_train, b_train_1)
+            loss_array = np.append(loss_array, [[dcc_loss, behaviour_loss, total_loss]], axis=0)
 
         avg_train_loss = loss_array.mean(axis=0)
 
         return avg_train_loss
-    
+
     def test_loop(self, test_loader):
         """
         Handles testing within a single epoch and logs losses
         """
         loss_array = np.zeros((0, 3))
         for x_test, b_test_1 in test_loader:
-            dcc_loss, behaviour_loss, total_loss = self.test_step(
-                x_test, 
-                b_test_1
-            )
-            loss_array = np.append(
-                loss_array, 
-                [[dcc_loss, behaviour_loss, total_loss]],
-                axis=0
-            )
+            dcc_loss, behaviour_loss, total_loss = self.test_step(x_test, b_test_1)
+            loss_array = np.append(loss_array, [[dcc_loss, behaviour_loss, total_loss]], axis=0)
 
         avg_test_loss = loss_array.mean(axis=0)
 
         return avg_test_loss
-    
 
-def train_model(
-    x_train, 
-    b_train_1, 
-    model, 
-    b_type, 
-    gamma, 
-    learning_rate, 
-    n_epochs, 
-    initialisation=None, 
-    validation_data=None, 
-    device=None, 
-    report_ray_tune=False, 
-    pca_file_save=False
-):
+
+def train_model(x_train, b_train_1, model, b_type, gamma, learning_rate, n_epochs, initialisation=None,
+                validation_data=None, device=None, report_ray_tune=False):
     """
     Training BunDLe Net
 
@@ -216,14 +175,12 @@ def train_model(
         gamma (float): Weight for the DCC loss component.
         learning_rate (float): Learning rate for the Adam optimiser.
         n_epochs (int): Number of training epochs.
-        initialisation (str): 'pca_init' or 'best_of_5_init' or 
-            tuple (n, n_epochs) for 'best_of_n_init'
+        initialisation (str): 'pca_init' or 'best_of_5_init' or tuple (n, n_epochs) for
+                                'best_of_n_init'
         validation_data: (x_test, b_test_1)
         device (torch.device): Device where the model should be trained.
-        report_ray_tune (bool): Reports validation loss per epoch to ray tune
-            for hyperparameter optimisiaton.
-        pca_file_save (bool): Whether save weights file or not in case of 
-            'pca_init'.
+        report_ray_tune (bool): reports validation loss per epoch to ray tune for
+                                hyperparameter optimisiaton
     Returns:
         numpy.ndarray: Array of loss values during training.
     """
@@ -236,55 +193,17 @@ def train_model(
 
     if validation_data is not None:
         x_test, b_test_1 = validation_data
-        test_loader = torch_batch_prep(
-            x_test, 
-            b_test_1, 
-            device=device, 
-            shuffle=False
-        )
+        test_loader = torch_batch_prep(x_test, b_test_1, device=device, shuffle=False)
 
     if initialisation == 'pca_init':
-        ret = pca_initialisation(
-            x_train, 
-            model.tau, 
-            model.latent_dim, 
-            device, 
-            pca_file_save 
-        )
-        if pca_file_save: # ret is file path of the weights
-            model.tau.load_state_dict(torch.load(ret))
-        else: # ret is encoder
-            model.tau.load_state_dict(ret.state_dict())
-
+        pca_model_path = pca_initialisation(x_train, model.tau, model.latent_dim, device)
+        model.tau.load_state_dict(torch.load(pca_model_path))
     elif initialisation == 'best_of_5_init':
-        model = best_of_5_runs(
-            x_train, 
-            b_train_1, 
-            model, 
-            b_type, 
-            gamma, 
-            learning_rate, 
-            validation_data, 
-            device
-        )
-
+        model = best_of_5_runs(x_train, b_train_1, model, b_type, gamma, learning_rate, validation_data, device)
     elif isinstance(initialisation, tuple):
-        model = best_of_n_runs(
-            initialisation[0], 
-            initialisation[1], 
-            x_train, 
-            b_train_1, 
-            model, 
-            b_type, 
-            gamma, 
-            learning_rate, 
-            validation_data, 
-            device
-        )
-
+        model = best_of_n_runs(initialisation[0], initialisation[1], x_train, b_train_1, model, b_type, gamma, learning_rate, validation_data, device)
     elif initialisation is None:
         pass
-
     else:
         raise ValueError(f"Unknown initialization method: {initialisation}")
 
@@ -306,8 +225,7 @@ def train_model(
                 ray.train.report({"epoch": epoch, "val_loss": test_loss[-1]})
 
 
-        desc = f"Loss [Markov, Behaviour, Total]: {np.round(train_loss, 4)}"
-        epochs.set_description(desc)
+        epochs.set_description("Loss [Markov, Behaviour, Total]: " + str(np.round(train_loss, 4)))
 
     train_history = np.array(train_history)
     test_history = np.array(test_history) if test_history is not None else None
@@ -329,10 +247,6 @@ def model_inference(x_, model):
 
     model.eval()
     with torch.no_grad():
-        y0_ = model.tau(
-            torch.tensor(x_[:, 0], dtype=torch.float, device=device)
-        ).cpu().numpy()
+        y0_ = model.tau(torch.tensor(x_[:, 0], dtype=torch.float, device=device)).cpu().numpy()
 
     return y0_
-
-
