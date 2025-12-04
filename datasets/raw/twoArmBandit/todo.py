e26b5d4ac29f510eac1fc7aa0eceeebfbed17e01
Kerim Atak <102679208+kerim325@users.noreply.github.com>
2025-12-04 17:01:11 +0100

    Update todo.py and gitignore

--- datasets/raw/twoArmBandit/todo.py
+++ datasets/raw/twoArmBandit/todo.py
@@ -1,3 +1,5 @@
-# TODO: change paths embedd into NCMCM package structure
-# TODO: Come up with new behavior, maybe continuous?
 # TODO: Check against overfitting. When shuffling b, is the manifold a dot?
+# TODO: Refactor: change paths, embed into NCMCM package structure
+# TODO: Write unit tests for each function
+# TODO: Find optimal hyperparameters for bundlenet
+# TODO: Analyze bifurcation structure of the manifold
