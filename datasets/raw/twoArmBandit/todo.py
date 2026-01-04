3e65554b10c7b969a6eadc5097a5dd1e3e3e54de
Kerim Atak <102679208+kerim325@users.noreply.github.com>
2026-01-04 02:56:14 +0100

    Add todos

--- datasets/raw/twoArmBandit/todo.py
+++ datasets/raw/twoArmBandit/todo.py
@@ -3,3 +3,6 @@
 # TODO: Write unit tests for each function
 # TODO: Find optimal hyperparameters for bundlenet
 # TODO: Analyze bifurcation structure of the manifold
+# TODO: Upsample data: worries about loosing information on decision making process since too few samples for chooding state
+# TODO: Rethink behavioural state, maybe fuse some states together
+# TODO: Add single run json info like runtime at the end
