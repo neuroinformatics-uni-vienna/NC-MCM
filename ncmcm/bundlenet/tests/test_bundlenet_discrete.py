2c83b2810e3f51fb0f3165ea80bcfc9f63f85479
QuackyPenguin <denisdk2000@gmail.com>
2026-06-17 14:58:46 +0200

    Fix test bugs: best_of_5_init validation_data, commented assert, tk backend, markov divide-by-zero

--- ncmcm/bundlenet/tests/test_bundlenet_discrete.py
+++ ncmcm/bundlenet/tests/test_bundlenet_discrete.py
@@ -120,20 +120,24 @@ def test_bundlenet_training_best_of_5_init():
     X = np.random.rand(50, 10)
     B = np.random.randint(5, size=(50,))
     X_, B_ = prep_data(X, B, win=3)
+    # split data
+    from ncmcm.bundlenet.utils import timeseries_train_test_split
+    X_train, X_test, B_train, B_test = timeseries_train_test_split(X_, B_)
     # Deploy BunDLe Net
     latent_dim = 3
-    num_behaviour = np.unique(B).shape[0]
-    model = BunDLeNet(latent_dim=latent_dim, num_behaviour=num_behaviour, input_shape=X_.shape)
+    num_behaviour = np.unique(B_train).shape[0]
+    model = BunDLeNet(latent_dim=latent_dim, num_behaviour=num_behaviour, input_shape=X_train.shape)
     n_epochs = 5
     loss_array, _ = train_model(
-        X_,
-        B_,
+        X_train,
+        B_train,
         model,
         b_type='discrete',
         gamma=0.9,
         learning_rate=0.001,
         n_epochs=n_epochs,
-        initialisation='best_of_5_init'
+        initialisation='best_of_5_init',
+        validation_data=(X_test, B_test)
     )
     assert loss_array.shape == (n_epochs, 3)
 
--- ncmcm/bundlenet/tests/test_bundlenet_subsystems.py
+++ ncmcm/bundlenet/tests/test_bundlenet_subsystems.py
@@ -62,28 +62,34 @@ def test_bundlenet_training():
 def test_bundlenet_training_best_of_5_init():
     X = np.random.rand(50, 10)
     B = np.random.randint(5, size=(50,))
+    
+    from ncmcm.bundlenet.utils import timeseries_train_test_split
 
     X_, B_ = prep_data(X, B, win=3)
     Xs_ = X_[:, :, :, [0,1,2]]
     Xi_ = X_[:, :, :, [3,4,5]]
     Xm_ = X_[:, :, :, [6,7,8,9]]
-    print(Xs_.shape, Xi_.shape, Xm_.shape)
+    
+    Xs_train, Xs_test, B_train, B_test = timeseries_train_test_split(Xs_, B_)
+    Xi_train, Xi_test, _, _ = timeseries_train_test_split(Xi_, B_)
+    Xm_train, Xm_test, _, _ = timeseries_train_test_split(Xm_, B_)
 
     # Deploy BunDLe Net
     latent_dim = 3
     num_behaviour = np.unique(B).shape[0]
-    input_shapes = Xs_.shape, Xi_.shape, Xm_.shape
+    input_shapes = Xs_train.shape, Xi_train.shape, Xm_train.shape
     model = BunDLeNet(latent_dim=latent_dim, num_behaviour=num_behaviour, input_shapes=input_shapes)
     n_epochs = 5
     loss_array, _ = train_model(
-        (Xs_, Xi_, Xm_),
-        B_,
+        (Xs_train, Xi_train, Xm_train),
+        B_train,
         model,
         b_type='discrete',
         gamma=0.9,
         learning_rate=0.001,
         n_epochs=n_epochs,
-        initialisation='best_of_5_init'
+        initialisation='best_of_5_init',
+        validation_data=((Xs_test, Xi_test, Xm_test), B_test)
     )
     assert loss_array.shape == (n_epochs, 3)
 
